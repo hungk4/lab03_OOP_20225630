80f6769201c079a53ea4379435f94361bdc9faa1
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-12-20 23:45:26 +0700

    chuong trinh painter

--- aimsProject/src/module-info.java
+++ aimsProject/src/module-info.java
@@ -6,4 +6,12 @@
  */
 module aims {
 	requires java.desktop;
+	requires javafx.graphics;
+	requires javafx.controls;
+    requires javafx.fxml;
+    
+
+    exports javafx;
+    exports test2; 
+	
 }
