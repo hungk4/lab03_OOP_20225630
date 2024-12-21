4d6d8ddeb7b0110d0a48188f27db1ee75ca4dda7
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-12-21 16:49:05 +0700

    update button in tableview

--- aimsProject/src/aims/screen/fxml/CartScreenController.java
+++ aimsProject/src/aims/screen/fxml/CartScreenController.java
@@ -1,7 +1,11 @@
 package aims.screen.fxml;
 import aims.cart.Cart;
 import aims.media.Media;
+import aims.media.Playable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button; // Thêm dòng này
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -10,6 +14,12 @@ public class CartScreenController {
 	
 	private Cart cart;
 	
+	@FXML
+    private Button btnPlay;
+
+    @FXML
+    private Button btnRemove;
+
     @FXML
     private TableView<Media> tblMedia;
 
@@ -35,8 +45,31 @@ public class CartScreenController {
     			new PropertyValueFactory<Media, String>("category"));
     	colMediaCost.setCellValueFactory(
     			new PropertyValueFactory<Media, Float>("cost"));
-    	
     	tblMedia.setItems(this.cart.getItemsOrdered());
+    	
+    	btnPlay.setVisible(false);
+    	btnRemove.setVisible(false);
+    	
+    	tblMedia.getSelectionModel().selectedItemProperty().addListener(
+    			new ChangeListener<Media>() {
+    				
+    				@Override
+    				public void changed(ObservableValue<? extends Media> observable, Media oldValue,
+    						Media newValue) {
+    					if(newValue != null) {
+    						updateButtonBar(newValue);
+    					}
+    				}
+    			});
+    }
+    
+    void updateButtonBar(Media media) {
+    	btnRemove.setVisible(true);
+    	if(media instanceof Playable) {
+    		btnPlay.setVisible(true);
+    	} else {
+    		btnPlay.setVisible(false);
+    	}
     }
 
 }
